ec6c0547fd5ab8665e4a9c6f91d6bbad0cb450ee
gigamaster <1905497+gigamaster@users.noreply.github.com>
2024-06-14 17:03:51 +0200

    Download link

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ import datetime as dt
 
 with open('/src/icons.json', encoding="utf-8") as json_file:
     data = json.load(json_file)
-    urepo= 'https://github.com/gigamaster/codemo/tree/main/data'
+    urepo= 'https://github.com/gigamaster/codemo/tree/main/data/'
 
 
 def main():
