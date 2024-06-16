8a91f42933c0fb5491bdf7b351d2d7fe826f2e05
gigamaster <1905497+gigamaster@users.noreply.github.com>
2024-06-16 07:31:00 +0200

    up lib

--- src/main.py
+++ src/main.py
@@ -10,68 +10,69 @@ import base64
 import datetime as dt
 
 with open('/src/icons.json', encoding="utf-8") as json_file:
-    data = json.load(json_file)
-    urepo= 'https://github.com/gigamaster/codemo/tree/main/data/'
-
+  data   = json.load(json_file)
+  urepo  = "https://github.com/gigamaster/codemo/tree/main/app/"
+  ufolder= "onclick=\"GitZip.zipRepo('https://github.com/gigamaster/codemo/tree/main/app/"
+  ufile  = "onclick=\"GitZip.zipRepo('https://github.com/gigamaster/codemo/blob/main/app/"
 
 def main():
-    """
-    main function
-    """
-    if len(sys.argv) > 1:
-        print("changing directory to " + sys.argv[1])
-        # add error handling to chdir
-        try:
-            os.chdir(sys.argv[1])
-        except OSError:
-            print("Cannot change the current working Directory")
-            sys.exit()
-    else:
-        print("no directory specified")
+  """
+  main function
+  """
+  if len(sys.argv) > 1:
+    print("changing directory to " + sys.argv[1])
+    # add error handling to chdir
+    try:
+        os.chdir(sys.argv[1])
+    except OSError:
+        print("Cannot change the current working Directory")
         sys.exit()
+  else:
+      print("no directory specified")
+      sys.exit()
 
-    for dirname, dirnames, filenames in os.walk('.'):
-        if 'index.html' in filenames:
-            print("index.html already exists, skipping...")
-        else:
-            print("index.html does not exist, generating")
-            with open(os.path.join(dirname, 'index.html'), 'w', encoding="utf-8") as f:
-                f.write("\n".join([
-                    get_template_head(dirname),
-                    "<tr class=\"w-2/4 border-b dark:border-primary-darker hover:bg-primary-100 dark:hover:bg-primary-dark\">" +
-                        "<th scope=\"row\" class=\"py-2 px-2 lg:px-6 font-medium whitespace-nowrap flex align-middle\">" +
-                        "<a class=\"flex flex-nowrap items-center my-auto dark:text-light\" href=\"../\">" +
-                        "<img style=\"max-width:23px; margin-right:5px\" src=\"" + get_icon_base64("o.folder-home") + "\"/>" +
-                        "<span class=\"updir\"></span></a></th><td class=\"text-center\">-</td><td class=\"text-center\">-</td>" +
-                        "<td class=\"text-center\">-</td></tr>" if dirname != "." else "",
-                        ]))
-                #sort dirnames alphabetically
-                dirnames.sort()
-                for subdirname in dirnames:
-                    f.write("<tr class=\"w-1/4 border-b dark:border-primary-darker hover:bg-primary-100 dark:hover:bg-primary-dark\">" +
-                            "<th scope=\"row\" class=\"py-2 px-2 lg:px-6 font-medium whitespace-nowrap flex align-middle\">" +
-                            "<a class=\"flex flex-nowrap items-center my-auto dark:text-light\" href=\"" + subdirname + "/\">" +
-                            "<img style=\"max-width:23px; margin-right:5px\" src=\"" + get_icon_base64("o.folder") + "\"/>" + 
-                            subdirname + "</a></th><td class=\"text-center\">-</td><td class=\"text-center\">-</td>" +
-                            "<td class=\"text-center\">" +
-                            "<a target=\"_blank\" href=\"https://download-directory.github.io/?url=" + urepo + subdirname + "/\" download><span class=\"download\"></span></td></a></tr>\n")
-                #sort filenames alphabetically
-                filenames.sort()
-                for filename in filenames:
-                    path = (dirname == '.' and filename or dirname +
-                            '/' + filename)
-                    f.write("<tr class=\"w-1/4 border-b dark:border-primary-darker hover:bg-primary-100 dark:hover:bg-primary-dark\">" +
-                            "<th scope=\"row\" class=\"py-2 px-2 lg:px-6 font-medium whitespace-nowrap flex align-middle\">" + 
-                            "<a class=\"flex flex-nowrap items-center my-auto dark:text-light\" href=\"" + filename + "\" target=\"_blank\">" 
-                            "<img style=\"max-width:23px; margin-right:5px\" src=\"" + get_icon_base64(filename) + "\"/>" +
-                            filename + "</a></th><td class=\"text-center\">" +
-                            get_file_size(path) + "</td><td class=\"text-center\">" + get_file_modified_time(path) + "</td>" +
-                            "<td class=\"text-center\">" +
-                            "<a target=\"_blank\" href=\"https://download-directory.github.io/?url=" + urepo + filename + "&filename="+ filename + "\" download=\"" + filename + "\"><span class=\"download\"></span></td></tr>\n")
+  for dirname, dirnames, filenames in os.walk('.'):
+      if 'index.html' in filenames:
+          print("index.html already exists, skipping...")
+      else:
+          print("index.html does not exist, generating")
+          with open(os.path.join(dirname, 'index.html'), 'w', encoding="utf-8") as f:
+              f.write("\n".join([
+                  get_template_head(dirname),
+                  "<tr class=\"w-2/4 border-b dark:border-primary-darker hover:bg-primary-100 dark:hover:bg-primary-dark\">" +
+                      "<th scope=\"row\" class=\"py-2 px-2 lg:px-6 font-medium whitespace-nowrap flex align-middle\">" +
+                      "<a class=\"flex flex-nowrap items-center my-auto dark:text-light\" href=\"../\">" +
+                      "<img style=\"max-width:23px; margin-right:5px\" src=\"" + get_icon_base64("o.folder-home") + "\"/>" +
+                      "<span class=\"updir\"></span></a></th><td class=\"text-center\">-</td><td class=\"text-center\">-</td>" +
+                      "<td class=\"text-center\">-</td></tr>" if dirname != "." else "",
+                      ]))
+              #sort dirnames alphabetically
+              dirnames.sort()
+              for subdirname in dirnames:
+                  f.write("<tr class=\"w-1/4 border-b dark:border-primary-darker hover:bg-primary-100 dark:hover:bg-primary-dark\">" +
+                          "<th scope=\"row\" class=\"py-2 px-2 lg:px-6 font-medium whitespace-nowrap flex align-middle\">" +
+                          "<a class=\"flex flex-nowrap items-center my-auto dark:text-light\" href=\"" + subdirname + "/\">" +
+                          "<img style=\"max-width:23px; margin-right:5px\" src=\"" + get_icon_base64("o.folder") + "\"/>" + 
+                          subdirname + "</a></th><td class=\"text-center\">-</td><td class=\"text-center\">-</td>" +
+                          "<td class=\"text-center\">" +
+                          "<a class=\"download\" onclick=\"GitZip.zipRepo('https://github.com/gigamaster/codemo/tree/main/app/" + subdirname + "');\" title=\"Download Folder\"><span class=\"download\"></span></td></a></tr>\n")
+              #sort filenames alphabetically
+              filenames.sort()
+              for filename in filenames:
+                  path = (dirname == '.' and filename or dirname +
+                          '/' + filename)
+                  f.write("<tr class=\"w-1/4 border-b dark:border-primary-darker hover:bg-primary-100 dark:hover:bg-primary-dark\">" +
+                          "<th scope=\"row\" class=\"py-2 px-2 lg:px-6 font-medium whitespace-nowrap flex align-middle\">" + 
+                          "<a class=\"flex flex-nowrap items-center my-auto dark:text-light\" href=\"" + filename + "\" target=\"_blank\">" 
+                          "<img style=\"max-width:23px; margin-right:5px\" src=\"" + get_icon_base64(filename) + "\"/>" +
+                          filename + "</a></th><td class=\"text-center\">" +
+                          get_file_size(path) + "</td><td class=\"text-center\">" + get_file_modified_time(path) + "</td>" +
+                          "<td class=\"text-center\">" +
+                          "<a class=\"download\" " + ufile + filename + "');\" title=\"Download File\"><span class=\"download\"></span></td></tr>\n")
 
-                f.write("\n".join([
-                    get_template_foot(),
-                ]))
+              f.write("\n".join([
+                  get_template_foot(),
+              ]))
 
 
 def get_file_size(filepath):
