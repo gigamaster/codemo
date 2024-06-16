0d7491fa76b9a6d305354efdb8f2475c3a59c3ab
gigamaster <1905497+gigamaster@users.noreply.github.com>
2024-06-17 01:47:26 +0200

    test notification

--- app/asset/lib/lib.module.js
+++ app/asset/lib/lib.module.js
@@ -3,10 +3,3 @@
 import * as FileSaver from "./filesaver.js";
 import * as JSZip from "./jszip.min.js";
 import * as api from "./api.js";
-
-window.notificationOptions = {
-    wrapperId: 'notificationWrapper',
-    templateFile: 'https://gigamaster.github.io/codemo/asset/notification.html',
-    autoClose: 3000,
-    autoRemove: 4000,
-  }
